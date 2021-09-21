c75ce4648d3439f24f7a8217ce5abea1ab965faf
Jason Chen <j.chen2303@gmail.com>
2021-09-20 20:55:57 -0400

    added loading text

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,31 +4,45 @@ import { NASA_HEADER, NASA_TOKEN } from '../secrets';
 const Content = () => {
   const [astronomiesState, setAstronomiesState] = useState([]);
   const [like, setLike] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    fetch(NASA_HEADER + NASA_TOKEN + '&count=5')
+    setLoading(true);
+    fetch(NASA_HEADER + NASA_TOKEN + '&count=50')
       .then((res) => res.json())
       .then((data) => {
         setAstronomiesState(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
+
+  if (loading) {
+    return (
+      <div>
+        Rendering Spaces, Planets, and other out-of-worldly resources...
+      </div>
+    );
+  }
 
   return (
     <div class="astronomies">
       {console.log(astronomiesState)}
 
-      <ul>
-        {astronomiesState.map((astronomy, idx) => (
-          <li key={idx}>
-            <h3>{astronomy.title}</h3>
-            <img src={astronomy.url} alt={astronomy.title} />
-            <div>{astronomy.explanation}</div>
-            <button onClick={() => setLike(!like)}>
-              {like ? 'Unlike' : 'Like'}
-            </button>
-          </li>
-        ))}
-      </ul>
+      {astronomiesState.map((astronomy, idx) => (
+        <div id="singleAstronomy" key={idx}>
+          <h3 id="image title">{astronomy.title}</h3>
+          <img src={astronomy.url} alt={astronomy.title} />
+          <div id="description">{astronomy.explanation}</div>
+          <button onClick={() => setLike(!like)}>
+            {like ? 'Unlike' : 'Like'}
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
