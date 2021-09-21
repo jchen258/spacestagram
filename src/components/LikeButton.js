65e005267d7188c9b49d77ba1f5ec4a3e0a6bee7
Jason Chen <j.chen2303@gmail.com>
2021-09-21 04:04:09 -0400

    likebutton component created

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 const LikeButton = () => {
   const [like, setLike] = useState(false);
 
   return (
-    <button onClick={() => setLike(!like)}>{like ? 'Unlike' : 'Like'}</button>
+    <button onClick={() => setLike(!like)}>{like ? 'unlike' : 'like'}</button>
   );
 };
 
